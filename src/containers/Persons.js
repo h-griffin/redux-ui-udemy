d64a8564106656fcb77bae70be62be7c2523dcf6
Griffin <60614863+h-griffin@users.noreply.github.com>
2020-09-29 11:45:13 -0700

    first commit

---
+++ src/containers/Persons.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+import Person from '../components/Person/Person';
+import AddPerson from '../components/AddPerson/AddPerson';
+
+import * as actionTypes from '../store/actions';
+import {connect } from 'react-redux';
+
+
+class Persons extends Component {
+
+    render () {
+        return (
+            <div>
+                <AddPerson personAdded={this.props.onAddedPerson} />
+                {this.props.prs.map(person => (
+                    <Person 
+                        key={person.id}
+                        name={person.name} 
+                        age={person.age} 
+                        clicked={() => this.props.onRemovedPerson(person.id)}/>
+                ))}
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        // map slice of redux state to access as prop in container
+        prs: state.persons
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onAddedPerson: () => dispatch( { type: actionTypes.ADD_PERSON }),
+        onRemovedPerson: (id) => dispatch( { type: actionTypes.REMOVE_PERSON, personId: id})
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Persons);
